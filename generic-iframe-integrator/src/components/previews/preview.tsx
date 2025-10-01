ae28571d7207e14c07b55f53541a0dd575290de9
Philipp Hofmann <philipp.hofmann@commercetools.com>
2025-10-01 12:13:00 +0200

    simplified

--- generic-iframe-integrator/src/components/previews/preview.tsx
+++ generic-iframe-integrator/src/components/previews/preview.tsx
@@ -1,13 +1,7 @@
-import { InfoMainPage } from '@commercetools-frontend/application-components';
-import messages from './messages';
-import { useIntl } from 'react-intl';
-
-import Spacings from '@commercetools-uikit/spacings';
 import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
 import LoadingSpinner from '@commercetools-uikit/loading-spinner';
 
 const Preview = () => {
-  const intl = useIntl();
   const externalUrl = useApplicationContext(({ environment }) => {
     if ('externalUrl' in environment) {
       return environment.externalUrl as string;
@@ -20,38 +14,32 @@ const Preview = () => {
   }
 
   return (
-    <InfoMainPage title={intl.formatMessage(messages.title)}>
-      <Spacings.Stack scale={'m'}>
-        <div
-          style={{
-            width: 1920,
-            height: 1080,
-            transform: 'scale(0.5)',
-            boxShadow: '0 2px 6px 0 rgba(63, 63, 63, 0.6)',
-            transition: 'box-shadow 0.25s',
-            borderRadius: '20px',
-            borderWidth: '20px 3px',
-            backgroundColor: '#3f3f3f',
-            margin: '0 auto',
-            transformOrigin: 'left top',
-          }}
-        >
-          <iframe
-            frameBorder={0}
-            loading={'lazy'}
-            width={'100%'}
-            height={'100%'}
-            style={{
-              width: 1880,
-              height: 1040,
-              margin: '20px',
-              backgroundColor: '#fff',
-            }}
-            src={externalUrl}
-          ></iframe>
-        </div>
-      </Spacings.Stack>
-    </InfoMainPage>
+    <div
+      style={{
+        width: '100%',
+        aspectRatio: '16 / 9',
+        // boxShadow: '0 2px 6px 0 rgba(63, 63, 63, 0.6)',
+        transition: 'box-shadow 0.25s',
+        // borderRadius: '20px',
+        // borderWidth: '20px 3px',
+        // backgroundColor: '#3f3f3f',
+        margin: '0 auto',
+        transformOrigin: 'left top',
+      }}
+    >
+      <iframe
+        frameBorder={0}
+        loading={'lazy'}
+        width={'100%'}
+        height={'100%'}
+        style={{
+          width: 'calc(100%)',
+          height: 'calc(100%)',
+          backgroundColor: '#fff',
+        }}
+        src={externalUrl}
+      ></iframe>
+    </div>
   );
 };
 Preview.displayName = 'Preview';
